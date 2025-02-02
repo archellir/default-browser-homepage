cce15d423884f66a41dc86e408d7eacd0a97de36
arcbjorn <arky.happ@gmail.com>
2025-02-02 09:52:55 -0300

    feat(lib): add Search component

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,11 @@
+<div class="mb-8">
+	<form action="https://google.com/search" method="get" target="_blank" rel="noopener noreferrer">
+		<input
+			type="text"
+			name="q"
+			placeholder="Search Google..."
+			autocomplete="off"
+			class="w-96 rounded-lg bg-white/20 px-4 py-3 text-white backdrop-blur-md transition-colors placeholder:text-white/70 focus:bg-white/30 focus:outline-none"
+		/>
+	</form>
+</div>
